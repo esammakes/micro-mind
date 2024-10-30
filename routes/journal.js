549f0a57334cc9a8fb4662c489a6840565e16c63
emsam18 <87667255+emsam18@users.noreply.github.com>
2024-10-29 23:14:03 -0400

    added edit and delete endpoints

--- routes/journal.js
+++ routes/journal.js
@@ -35,4 +35,46 @@ router.get("/allentries", auth, async (req, res) => {
   }
 });
 
+//route to edit a journal entry
+router.put("/edit/:id", auth, async (req, res) => {
+  const { title, content } = req.body;
+  try {
+    let journal = await Journal.findById(req.params.id);
+
+    if (!journal) {
+      return res.status(404).json({ msg: "Journal not found" });
+    }
+
+    //check if the journal belongs to current user
+    if (journal.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    //update the journal fields
+    journal = await Journal.findByIdAndUpdate(
+      req.params.id,
+      { $set: { title, content } },
+      { new: true } //return the updated document
+    );
+
+    res.json(journal);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+router.delete("/delete/:id", auth, async (req, res) => {
+  try {
+    const deletedJournal = await Journal.findByIdAndDelete(req.params.id);
+    if (!deletedJournal) {
+      return res.status(404).json({ msg: "Journal entry not found" });
+    }
+    res.json({ msg: "Journal entry deleted" });
+  } catch (err) {
+    console.error("Error deleting journal entry: ", err);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
